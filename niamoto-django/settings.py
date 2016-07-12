b3c61c2c4bff877eae1cc9547cb0ef0d792e3203
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-07-12 16:03:49 +1100

    Change rest default security for niamoto-django

--- niamoto-django/settings.py
+++ niamoto-django/settings.py
@@ -172,7 +172,6 @@ REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
