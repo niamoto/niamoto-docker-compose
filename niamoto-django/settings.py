806c80781483326a7a18506a9d359330335489e1
Dimitri Justeau <dimitri.justeau@gmail.com>
2016-12-14 11:20:49 +1100

    Update niamoto-django settings

--- niamoto-django/settings.py
+++ niamoto-django/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'apps.forest_digitizing',
     'apps.inventories',
     'apps.taxon_dashboards',
+    'apps.plot_dashboard',
     'rest',
     'web',
 
@@ -251,7 +252,6 @@ REST_FRAMEWORK = {
         'rest_framework.filters.OrderingFilter',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 100,
 }
 
 REST_FRAMEWORK_DOCS = {
