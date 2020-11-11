3f0708e3d31c6b3586dd367e24da7bc7c12d3ab7
TM <tmangeard@gmx.fr>
2020-11-12 09:35:03 +1100

    rename imagfe django local

--- niamoto-django-local/reload_image.sh
+++ niamoto-django-local/reload_image.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 cd $NIAMOTO_COMPOSE
 sudo docker-compose down -v
-sudo docker rmi niamoto-docker-compose_niamoto-nginx niamoto-docker-compose_niamoto-django niamoto-docker-compose_niamoto-postgres
+sudo docker rmi niamoto-django-local_niamoto-django niamoto-django-local_niamoto-nginx
 sudo docker-compose up -d --build
 
